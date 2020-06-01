cf8e92fe3afbae11f7983b989766dfcaa79d6cd5
Claudio Matos~ <caupath16@gmail.com>
2020-06-01 10:53:07 -0300

    OK

--- app/route/user/user.route.js
+++ app/route/user/user.route.js
@@ -5,6 +5,6 @@ const userController = require('../../controller/user/user.controller')
 
 router.post('/createUser', userController.saveUser)
 router.put('/updateUser', userController.updateUser)
-router.get('/test', userController)
+router.get('/test', userController.test)
 
 module.exports = router;
